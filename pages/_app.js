51a6bd352b3240b28cf2f43759a305d23ed9bb40
Rakhat Alpysbai <rekebek02@gmail.com>
2021-05-08 12:51:00 +0600

    init app template

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+   return (
+      <>
+         <Head>
+            <link
+               href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
+               rel="stylesheet"
+            />
+            <link
+               href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+               rel="stylesheet"
+            />
+            <meta
+               name="description"
+               content="Hi there :), I'm Rakhat, Web developer from Nur-Sultan, KZ"
+            />
+            <meta
+               name="keywords"
+               content="Web developer, Frontend, Frontend Developer, JavaScript"
+            />
+            <title>Rakhat Alpysbai</title>
+         </Head>
+         <Component {...pageProps} />
+      </>
+   );
 }
 
-export default MyApp
+export default MyApp;
