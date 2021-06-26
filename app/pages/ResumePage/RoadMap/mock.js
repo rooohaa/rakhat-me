851a8e0f2c792cfbd220356237c683c3aa845e82
Rakhat Alpysbai <rekebek02@gmail.com>
2021-06-26 17:32:24 +0600

    update resume

--- app/pages/ResumePage/RoadMap/mock.js
+++ app/pages/ResumePage/RoadMap/mock.js
@@ -18,20 +18,27 @@ export const education = [
 export const exp = [
    {
       id: 1,
-      year: '2021 - Current',
+      year: 'Current',
+      place: 'Somnium LLP',
+      position: 'Frontend Engineer',
+      descr: 'Currently working as a Frontend Engineer on EDO Document flow project with team.',
+   },
+   {
+      id: 2,
+      year: '2021',
       place: 'Avex Agency',
       position: 'Junior Frontend Developer',
       descr: 'I worked for half a year at an IT product development agency. I got good experience in creating modern with new web technologies.',
    },
    {
-      id: 2,
+      id: 3,
       year: '2020-2021',
       place: 'Own Projects',
       position: 'Web Developer',
       descr: 'In parallel with the studying, I implemented my own projects. With this i gained development experience. You can view some of my projects in Github.',
    },
    {
-      id: 3,
+      id: 4,
       year: '2019-2021',
       place: 'Hackathons',
       position: 'Team member & Developer',
